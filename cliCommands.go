1a0312c77fdb1777af3ad0f920198b7cc88d45a5
hamyqueso <jonathan.cheung09@gmail.com>
2025-08-27 13:16:56 -0700

    completed login handler function and added run and register command functions

--- cliCommands.go
+++ cliCommands.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/hamyqueso/blogGator/internal/config"
 )
@@ -18,15 +19,29 @@ type command struct {
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) == 0 {
 		return errors.New("login command requires username argument")
+	} else if len(cmd.args) > 1 {
+		return errors.New("login command takes one username argument, too many arguments")
 	}
 
 	username := cmd.args[0]
 
-	s.cfg.CurrentUserName = username
+	if err := s.cfg.SetUser(username); err != nil {
+		return err
+	}
+
+	fmt.Println("Username has been set in config file")
 
 	return nil
 }
 
 type commands struct {
 	handlers map[string]func(*state, command) error
+}
+
+func (c *commands) run(s *state, cmd command) error {
+	return c.handlers[cmd.name](s, cmd)
+}
+
+func (c *commands) register(name string, f func(*state, command) error) {
+	c.handlers[name] = f
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/hamyqueso/blogGator/internal/config"
 )
@@ -10,10 +11,27 @@ func main() {
 	c, err := config.Read()
 	if err != nil {
 		fmt.Printf("%v\n", err)
+		os.Exit(1)
 	}
 
-	if err = c.SetUser("jonny"); err != nil {
+	// if err = c.SetUser("jonny"); err != nil {
+	// 	fmt.Printf("%v\n", err)
+	// }
+
+	s := &state{cfg: &c}
+	if len(os.Args) < 2 {
+		fmt.Println("Need at least 2 arguments")
+		os.Exit(1)
+	}
+	cmd := command{name: os.Args[1], args: os.Args[2:]}
+	cmds := commands{handlers: make(map[string]func(*state, command) error)}
+
+	cmds.register("login", handlerLogin)
+
+	err = cmds.run(s, cmd)
+	if err != nil {
 		fmt.Printf("%v\n", err)
+		os.Exit(1)
 	}
 
 	c, err = config.Read()
